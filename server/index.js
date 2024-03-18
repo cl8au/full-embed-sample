92ff6dfc5464b218400e0c410c7d9990edd77a18
Calvin Liu <calvin.liu@zendesk.com>
2024-03-18 15:29:44 +1100

    Use path instead of the value of private key

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import {nanoid} from 'nanoid';
 import {sign} from 'jsonwebtoken';
 import {basic as createBasicAuth} from 'http-auth';
 import auth from 'http-auth-connect';
+import fs from 'fs';
 
 const basicAuth = createBasicAuth({realm: "Private Area"}, (username, password, callback) => {
   callback(username === process.env.USER_NAME && password === process.env.USER_PASSWORD);
@@ -15,18 +16,20 @@ export default app => {
   app.get('/workato-jwt', (req, res) => {
     const {WK_API_KEY, WK_CUSTOMER_ID, WK_USER_ID, WK_CUSTOM_VENDOR_ORIGIN} = process.env;
     const subParams = [WK_API_KEY, WK_CUSTOMER_ID];
-
     if (WK_USER_ID) {
       subParams.push(WK_USER_ID);
     }
 
+    const subValue = subParams.join(':');
+    console.log(`sub value: ${subValue}`)
+    const privateKey = fs.readFileSync(process.env.WK_JWT_PRIVATE_KEY_PATH, 'utf8');
     const token = sign(
       {
-        sub: subParams.join(':'),
+        sub: subValue,
         jti: nanoid(),
         origin: WK_CUSTOM_VENDOR_ORIGIN || undefined
       },
-      process.env.WK_JWT_PRIVATE_KEY,
+      privateKey,
       {algorithm: 'RS256'}
     );
 
